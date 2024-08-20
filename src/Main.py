b1a7aff1ff84cfa893f93238c8329fc9e254be3a
brunosouza0103 <bruno.souza@edu.unifil.br>
2024-08-19 22:07:39 -0300

    rascunho menu inicial

---
+++ src/Main.py
@@ -0,0 +1,4 @@
+def menuInicial():
+    print("Escolha o que você deseja fazer\n 1. Cadastrar Funcionário\n 2. Cadastrar Equipamento\n 3. Emprestar Equipamento\n 4. Devolver Equipamento\n 5. Listar Equipamentos\n")
+    menu = int(input("Digite uma opção:"))
+menuInicial()
