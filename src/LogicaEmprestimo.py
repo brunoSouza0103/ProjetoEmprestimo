9119dfc515b92c9ee96997bd17fbce965479a509
Laboratório Informática <labinfo@unifil.br>
2024-08-23 19:56:45 -0300

    LogicaEmprestimo

---
+++ src/LogicaEmprestimo.py
@@ -0,0 +1,18 @@
+print("Voce escolheu emprestar um equipamento.\n")
+
+nome = str(input("Digite seu nome:"))
+cpf = str(input("Informe seu CPF:"))
+
+print("Lista de equipamentos: \n")
+#print(equipamentos[], \n)
+cod = str(input("Digite o código do equipamento que você deseja emprestar."))
+
+
+
+
+
+
+
+
+
+    
