0d6f60d30873b77243b94ec4d5305658fe31db9b
LuizHillebrande <luizhill@edu.unifil.br>
2024-08-23 19:58:40 -0300

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -1,26 +1,40 @@
 matriz = []
 
-def adicionar_linha(matriz, nome, status='disponível', responsavel=None):
-    matriz.append([nome, status, responsavel])
+def adicionar_linha(matriz, codigo, status='disponível', responsavel=None, situacao='novo'):
+    matriz.append([codigo, status, responsavel, situacao ])
 
 while True:
-    nome = input("Digite o nome do equipamento (ou 'sair' para encerrar): ").strip()
+    codigo = input("Digite o código do equipamento (ou 'sair' para encerrar): ").strip()
    
-    if nome.lower() == 'sair':
+    if codigo.lower() == 'sair':
         break
    
-    entrada = input("Digite o status (d para disponível ou i para indisponível): ").strip().lower()
+    entrada = input("Digite o status do equipamento (d para Disponível, e para Emprestado, m para Manutenção): ").strip().lower()
     if entrada == 'd':
         status = 'disponível'
     elif entrada == 'i':
         status = 'indisponível'
+    elif entrada == 'm':
+        status = 'manutenção'
     else:
-        print("Status inválido. Por favor, digite 'd' para disponível ou 'i' para indisponível.")
+        print("Status inválido. Por favor, digite 'd' para disponível, 'i' para indisponível, 'm' para manutenção: ")
         continue  
    
     responsavel = input("Digite o nome do responsável (deixe em branco se não houver): ").strip() or None
+    estado = input("Digite o estado do equipamento (letra n para novo, u para usado, d para defeito, b para baixado: ")
+    if estado == 'n':
+        situacao = 'novo'
+    if estado == 'u':
+        situacao = 'usado'
+    if estado =='d':
+        situacao = 'defeito'
+    if estado =='b':
+        situacao ='baixado'
+    else:
+        print("Estado inválido. Por favor, digite 'n' para novo, 'u' para usado, 'd' para defeito ou 'b' para baixado")
+        continue 
    
-    adicionar_linha(matriz, nome, status, responsavel)
+    adicionar_linha(matriz, codigo, status, responsavel, situacao)
    
     print("\nMatriz atual:")
     for linha in matriz:
@@ -29,5 +43,4 @@ while True:
 print("\nMatriz final:")
 for linha in matriz:
     print(linha)
-
 
